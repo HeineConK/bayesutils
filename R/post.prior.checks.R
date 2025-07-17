1ae28a5af21d938a23c116f472e75a913c667ddc
Florian Heinke <111225408+HeineConK@users.noreply.github.com>
2025-07-17 11:12:01 +0200

    use internal mcmc.gather

--- R/post.prior.checks.R
+++ R/post.prior.checks.R
@@ -1,127 +1,62 @@
 #' @export post.prior.check
 #' @export
-post.prior.check <- function(prior_draws, post_draws, qseq = seq(.1, .9, .1)) {
-  post_vars <- draws.varnames(post_draws)
-  prior_vars <- draws.varnames(prior_draws)
+post.prior.check <- function(prior_draws, post_draws, qseq = seq(.1, .9, .1), vars = NULL) {
 
-  postd_list <- list()
-
-  k <- 1
-  for (vi in 1:length(post_draws)) {
-    di <- post_draws[[vi]]
-    dv <- dim(di)
-    if (is.null(dv)) {
-      stop(paste0("Draws missing in ", names(post_draws)[vi]))
-    }
-    if (length(dv) == 2) {
-      if (dv[2] == 1) {
-        postd_list[[k]] <- di[, 1]
-        k <- k + 1
-      }
-      if (dv[2] > 1) {
-        for (i in 1:dv[2]) {
-          postd_list[[k]] <- di[, i]
-          k <- k + 1
-        }
-      }
-      if (length(dv) == 3) {
-        for (i in 1:dv[2]) {
-          for (j in 1:dv[3]) {
-            postd_list[[k]] <- di[, i, j]
-            k <- k + 1
-          }
-        }
-      }
-    }
-  }
-
-  names(postd_list) <- post_vars
-
-
-  # prior draws
-  priord_list <- list()
-  k <- 1
-  for (vi in 1:length(prior_draws)) {
-    di <- prior_draws[[vi]]
-    dv <- dim(di)
-    if (is.null(dv)) {
-      stop(paste0("Draws missing in ", names(prior_draws)[vi]))
-    }
-    if (length(dv) == 2) {
-      if (dv[2] == 1) {
-        priord_list[[k]] <- di[, 1]
-        k <- k + 1
-      }
-      if (dv[2] > 1) {
-        for (i in 1:dv[2]) {
-          priord_list[[k]] <- di[, i]
-          k <- k + 1
-        }
-      }
-      if (length(dv) == 3) {
-        for (i in 1:dv[2]) {
-          for (j in 1:dv[3]) {
-            priord_list[[k]] <- di[, i, j]
-            k <- k + 1
-          }
-        }
-      }
-    }
-  }
-
-  names(priord_list) <- prior_vars
-
-  if (length(priord_list) != length(postd_list)) {
+  df_pri <- as.data.frame(..mcmc.gather(draws = prior_draws, vars = vars, gather = "matrix")$draws_mat)
+  df_pos <- as.data.frame(..mcmc.gather(draws = post_draws, vars = vars, gather = "matrix")$draws_mat)
+  
+  if(ncol(df_pri) != ncol(df_pos)){
     warning(paste0(
       "Missmatch in variable numbers. \nPrior: ",
-      length(priord_list),
+      length(ncol(df_pri)),
       " Post: ",
-      length(postd_list)
+      length(ncol(df_pos))
     ))
   }
-
-  Nvars_prior <- length(priord_list)
-
-  checks <- lapply(1: Nvars_prior, function(vi) {
-    vn <- names(priord_list)[vi]
-    d1 <- priord_list[[vn]]
-    d2 <- postd_list[[which(names(postd_list) == vn)]]
-
+  
+  Nvars_prior <- ncol( df_pri )
+  checks <- lapply(1:Nvars_prior, function(vi) {
+    vn <- names(df_pri)[vi]
+    d1 <- df_pri[,vn]
+    d2 <- df_pos[,vn]
+    
     pi50_1 <- PI(d1, 0.5)
     pi50_2 <- PI(d2, 0.5)
-
+    
     d1q <- quantile(d1, qseq)
     d2q <- sapply(d1q, function(quant)
       sum(d2 < quant)) / length(d2)
     qq_sq_residuals <- sum((d2q - qseq)^2)
-
+    
     qq_sq_residuals_max <- sum(qseq^2)
-
+    
     list(
       qq_points = cbind(quantile_prior = qseq, quantile_post = d2q),
       qq_sqr_residuals = qq_sq_residuals,
       qq_norm_sqr_residuals = qq_sq_residuals / qq_sq_residuals_max
     )
   })
-
-  names(checks) <- names( priord_list )
-
+  
+  names(checks) <- names( df_pri )
+  
   qq_nsq_resids <- sapply(checks, \(ch) ch$qq_norm_sqr_residuals )
   names(qq_nsq_resids) <- names(checks)
-
+  
   resid_order <- order( qq_nsq_resids )
   imax <- ifelse(Nvars_prior> 10, 10, Nvars_prior)
-
-
+  
+  
   checks$summary__ <- list(
     sum_resids = summary(qq_nsq_resids),
     min10_resids = qq_nsq_resids[ resid_order[1:imax] ]
   )
-
+  
   class(checks) <- "post.prior.check"
-
+  
   return( checks )
 }
+
+
 
 
 #' @export print.post.prior.check
